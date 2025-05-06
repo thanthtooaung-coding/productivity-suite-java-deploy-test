87c4bf5f79a9b09637d97ee8b11b95fe8883b739
Aung Khaing Khant <aungkhaingkhant.dev@gmail.com>
2025-05-06 10:00:21 +0630

    initial setup

---
+++ src/main/java/com/_p1m/productivity_suite/ProductivitySuiteApplication.java
@@ -0,0 +1,13 @@
+package com._p1m.productivity_suite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductivitySuiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductivitySuiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/_p1m/productivity_suite/ProductivitySuiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com._p1m.productivity_suite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductivitySuiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
